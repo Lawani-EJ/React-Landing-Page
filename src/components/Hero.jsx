35a82e7ad55de1c9ed828d351053f79cae9379ae
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-06 14:52:28 +0100

    Hero section component In this commit i made a hero component, Made with a large header text, Added with sign up button, Styling the hero section with CSS.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/hero.css';
+
+function Hero() {
+  return (
+    <div className='heroImg'>
+      <div className="heroText">
+        <h1 style={{ fontSize: '50px' }}>Sport Website</h1>
+        <p>Sport Website</p>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
+
